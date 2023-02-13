77b344beb6d7ce95a7c918b6fff0099735a7b4c3
VictoriaT87 <victoriaemt@gmail.com>
2023-02-13 11:17:22 +0000

    Added Procfile

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -23,11 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 
+# SECURITY WARNING: keep the secret key used in production secret!
 if os.path.isfile('env.py'):
     import env
 SECRET_KEY = os.environ.get('SECRET_KEY')
-
-# SECURITY WARNING: keep the secret key used in production secret!
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
